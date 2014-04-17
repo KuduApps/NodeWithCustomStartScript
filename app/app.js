a142e44b840a766a6a11a9b65112eb4a3108c718
Mingwei Tian <mingwei.tian@microsoft.com>
2014-04-17 16:37:23 -0700

    Require node version 0.10.5 as in wiki, so the test can run locally.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,5 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello Main App!\nprocess.version: ' + process.version + "\nprocess.env.NODE_ENV: " + process.env.NODE_ENV);
-  var REQUIRE_NODE_ENV = require("node-env");
-  res.write("\nrequire NODE_ENV: " + REQUIRE_NODE_ENV);
   res.end();
 }).listen(port);
