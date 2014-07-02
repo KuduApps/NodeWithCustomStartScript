847e476da4ed3e9a01d13bef7fb611562b0c72fb
Mingwei Tian <mingwei.tian@microsoft.com>
2014-07-02 11:03:23 -0700

    Updated tests.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Main App!\nprocess.version: ' + process.version + "\nprocess.env.NODE_ENV: " + process.env.NODE_ENV);
+  res.writeHead(200, { "Content-Type": 'text/plain' });
+  res.write("Hello!\n");
+  res.write("process.version: " + process.version + "\n");
+  res.write("process.env.NODE_ENV: " + process.env.NODE_ENV);
+  res.write("__filename: " + __filename + "\n");
+  res.write("__dirname: " + __dirname + "\n");
   res.end();
 }).listen(port);
